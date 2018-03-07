29ab079a1ab92add6879ae9bb391868b4461e8ef
vmurad <vmurad_5@mail.ru>
2018-03-08 00:59:20 +0300

    tlg-chat

---
+++ assets/js/tlgchat.js
@@ -0,0 +1,5 @@
+
+	var __REPLAIN_ = '5f0e5105-2f7c-4e69-967b-defd72d938c0';
+	(function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
+	var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+	})('https://widget.replain.cc/dist/client.js');
